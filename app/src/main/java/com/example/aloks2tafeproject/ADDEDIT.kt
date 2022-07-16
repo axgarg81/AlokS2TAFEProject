1197761850845daeea70adca856fc6a1205c9bd9
Alok <alok.garg@gmail.com>
2022-07-16 19:02:40 +1000

    New commit

---
+++ app/src/main/java/com/example/aloks2tafeproject/ADDEDIT.kt
@@ -0,0 +1,12 @@
+package com.example.aloks2tafeproject
+
+import android.app.Activity
+import android.os.Bundle
+
+class ADDEDIT: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.addedit)
+
+    }
+}
--- app/src/main/java/com/example/aloks2tafeproject/MainActivity.kt
+++ app/src/main/java/com/example/aloks2tafeproject/MainActivity.kt
@@ -1,13 +1,45 @@
 package com.example.aloks2tafeproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    private val MENUADD = Menu.FIRST+1
+    private val MENUEDIT = Menu.FIRST+2
+    private val MENUREMOVE = Menu.FIRST+3
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menu.add(Menu.NONE,MENUADD,Menu.NONE,"ADD")
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val goAdd = Intent(this,ADDEDIT::class.java)
+        startActivity(goAdd)
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menu.add(Menu.NONE,MENUEDIT,Menu.NONE,"EDIT")
+        menu.add(Menu.NONE,MENUREMOVE,Menu.NONE,"REMOVE")
+        super.onCreateContextMenu(menu, v, menuInfo)
+    }
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return super.onContextItemSelected(item)
     }
 }
